28bcd1785eab03c0832742ab93d99e73b2fd1435
Andrew Miller <55397229+andrew-miller182@users.noreply.github.com>
2021-07-07 13:04:22 -0600

    testing opening all origins

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5001;
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
