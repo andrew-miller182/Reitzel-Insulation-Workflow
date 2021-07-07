f1f46bf634c4632b4d495dc7bd704cc18329d230
Andrew Miller <55397229+andrew-miller182@users.noreply.github.com>
2021-07-07 11:38:47 -0600

    updated CORS policy to allow localhost

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5001;
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', async (req, res) => {
   res.send('Reitzel Server Running');
 })
