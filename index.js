9132876670113e2ace1db6af563cde58ea2510b9
Andrew Miller <55397229+andrew-miller182@users.noreply.github.com>
2021-07-12 14:12:39 -0600

    testing cors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/fetchValues', cors(), async (req, res, next) => {
   let condition = req.body.condition || 'true'
   let output = await myApi.fetchValues(tableName, columns, condition)
   
-  res.send(req.body);
+  res.send(output);
 })
 
 app.post('/insertValues', async (req, res, next) => {
