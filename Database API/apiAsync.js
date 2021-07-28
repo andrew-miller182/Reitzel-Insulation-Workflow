641d9007eb7dbb98e9ae8abf6264d27423afebe7
Andrew Miller <55397229+andrew-miller182@users.noreply.github.com>
2021-07-28 10:08:04 -0600

    added connection releases test for max conn problem

--- Database API/apiAsync.js
+++ Database API/apiAsync.js
@@ -16,34 +16,40 @@ let insertValues = async (tableName, values) => {
 
   let sql = 'INSERT INTO ' + tableName + ' VALUES (' + values + ')'
   var [rows] = await conn.query(sql)
+  conn.release();
   return JSON.parse(JSON.stringify(rows))
+  
 }
 
 let updateValues = async (tableName, columnsAndValues, condition) => {
   var conn = await getConnection()
   let sql =
     'UPDATE ' + tableName + ' SET ' + columnsAndValues + ' WHERE ' + condition
-  var [rows] = await conn.query(sql)
+  var [rows] = await conn.query(sql);
+  conn.release();
   return JSON.parse(JSON.stringify(rows))
 }
 
 let deleteValues = async (tableName, condition) => {
   var conn = await getConnection()
   let sql = 'DELETE FROM ' + tableName + ' WHERE ' + condition
-  var [rows] = await conn.query(sql)
+  var [rows] = await conn.query(sql);
+  conn.release();
   return JSON.parse(JSON.stringify(rows))
 }
 
 let fetchValues = async (tableName, columns, condition) => {
   var conn = await getConnection()
   let sql = 'SELECT ' + columns + ' FROM ' + tableName + ' WHERE ' + condition
-  var [rows] = await conn.query(sql)
+  var [rows] = await conn.query(sql);
+  conn.release();
   return JSON.parse(JSON.stringify(rows))
 }
 
 let processCustomQuery = async (sql) => {
   var conn = await getConnection()
-  var [rows] = await conn.query(sql)
+  var [rows] = await conn.query(sql);
+  conn.release();
   return JSON.parse(JSON.stringify(rows))
 }
 
