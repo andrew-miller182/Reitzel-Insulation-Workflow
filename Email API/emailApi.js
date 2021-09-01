2804d305bc420c51e469435cc6cb7f4426f78d98
Andrew Miller <55397229+andrew-miller182@users.noreply.github.com>
2021-09-01 12:06:13 -0600

    change to filepath

--- Email API/emailApi.js
+++ Email API/emailApi.js
@@ -55,7 +55,7 @@ let sendEmailHtml = async (callback, _to, _subject, _html) => {
     html: _html,
     attachments: [
       {
-        path:'https://github.com/andrew-miller-dev/Reitzel-Insulation-Desktop/blob/main/src/assets/Customer%20Info%20Sheet%20_%20Reitzel%20Insulation.pdf'
+        path:'https://github.com/andrew-miller-dev/Reitzel-Insulation-Desktop/raw/main/src/assets/Customer%20Info%20Sheet%20_%20Reitzel%20Insulation.pdf'
       }
     ]
   }
