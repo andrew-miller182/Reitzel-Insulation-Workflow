ee71e9bb9ee499413a8282cef24fb73ea21a0a8b
Andrew Miller <55397229+andrew-miller182@users.noreply.github.com>
2022-10-10 12:55:34 -0600

    increased limit for emails

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.json({limit:"200mb"}))
 
 
 app.get('/', cors(), async (req, res, next) => {
